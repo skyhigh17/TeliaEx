d173337b1879bf99d81c353fc446b0e927230ba9
skyhigh17 <magnus156@gmail.com>
2022-01-20 12:38:21 +0200

    Täpsustus
    Täpsustus Lisatud

--- TeliaApp2/resources/views/welcome.blade.php
+++ TeliaApp2/resources/views/welcome.blade.php
@@ -27,7 +27,7 @@ Samamoodi võiks keelsust ka ekraanilt endalt saada muuta. Ehk nüüd kui ma ole
 /rakendus/ekraan1 aadressil, mis näitab Vaadet 2 vene keeles, siis saan selle näiteks ise inglise
 keelseks muuta ja haldusest on näha, et ekraan 1 kuvab vaade 2-te inglise keeles.
 
-<div><p><strong>Kasutusjuhend:</strong></p><p> Migreeri andmebaas ja seede vaadete tabel: <strong> php artisan db:seed vaade </strong></p></div>
+<div><p><strong>Kasutusjuhend:</strong></p><p> Migreeri andmebaas(mysql) ja seede vaadete tabel: <strong> php artisan db:seed vaade </strong></p></div>
 <div>Halduslehel sisesta andmed, peale seda saad liikuda vastavasse kliendi vaatesse(Ekraani) ja muuta Halduslehel olevaid Ekraane/vaateid,<br> 
 ekraanides saad navigeerida halduslehel sisestatud urli kaudu lihtsalt /ekraan1 näiteks</div>
 <div>Haldusleht asub siin-> <a href="{{url('/admin')}}">Admin Leht</a><div>
